7c96fdd8ac9b82246fe7cd417be258539e6dd8e9
Daniel Pyka <daniel.pyka@iais.fraunhofer.de>
2014-06-25 12:35:47 +0200

    ORA-5 minor firmware improvements, added tokengenerator

--- RobertaIsBlockly/src/main/java/de/fhg/iais/roberta/javaServer/resources/ErrorMessageReceiver.java
+++ RobertaIsBlockly/src/main/java/de/fhg/iais/roberta/javaServer/resources/ErrorMessageReceiver.java
@@ -2,8 +2,6 @@ package de.fhg.iais.roberta.javaServer.resources;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
@@ -16,10 +14,10 @@ public class ErrorMessageReceiver {
 
     @POST
     //@Consumes(MediaType.TEXT_PLAIN)
-    @Produces(MediaType.TEXT_PLAIN)
+    //@Produces(MediaType.TEXT_PLAIN)
     public Response handle(String errorMessage) {
         LOG.info("/error, " + errorMessage);
-        return Response.ok().build();
+        return Response.ok("test").build();
     }
 
 }
