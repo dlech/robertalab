f3b2ef526076fd9e7d163dc803e2a2de381b6865
Daniel Pyka <daniel.pyka@iais.fraunhofer.de>
2014-10-06 11:56:39 +0200

    ORA-219, ORA-138 json test on brick

--- OpenRobertaServer/src/main/java/de/fhg/iais/roberta/javaServer/resources/TokenReceiver.java
+++ OpenRobertaServer/src/main/java/de/fhg/iais/roberta/javaServer/resources/TokenReceiver.java
@@ -1,9 +1,13 @@
 package de.fhg.iais.roberta.javaServer.resources;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,10 +16,10 @@ public class TokenReceiver {
     private static final Logger LOG = LoggerFactory.getLogger(TokenReceiver.class);
 
     @POST
-    //@Consumes(MediaType.TEXT_PLAIN)
-    //@Produces(MediaType.TEXT_PLAIN)
-    public Response handle(String token) {
-        LOG.info("/token - " + token);
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response handle(JSONObject request) {
+        LOG.info("/token - " + request);
         return Response.ok("OK").build();
     }
 
