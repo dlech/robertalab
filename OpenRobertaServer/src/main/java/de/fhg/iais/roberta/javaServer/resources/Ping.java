ffea284714f28782faf379969623e25bf3635521
Reinhard Budde <reinhard.budde@iais.fraunhofer.de>
2014-11-24 22:23:41 +0100

    ORA-301 maven-way for EV3Menu, refactoring the poms and properties, rebuild ora.sh

--- OpenRobertaServer/src/main/java/de/fhg/iais/roberta/javaServer/resources/Ping.java
+++ OpenRobertaServer/src/main/java/de/fhg/iais/roberta/javaServer/resources/Ping.java
@@ -16,26 +16,33 @@ import org.slf4j.LoggerFactory;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
+import de.fhg.iais.roberta.brick.BrickCommunicator;
+import de.fhg.iais.roberta.javaServer.provider.OraData;
 import de.fhg.iais.roberta.util.ClientLogger;
+import de.fhg.iais.roberta.util.Util;
 
 @Path("/ping")
 public class Ping {
     private static final Logger LOG = LoggerFactory.getLogger(Ping.class);
     private final String version;
+    private final BrickCommunicator brickCommunicator;
 
     @Inject
-    public Ping(@Named("version") String version) {
+    public Ping(@Named("version") String version, BrickCommunicator brickCommunicator) {
         this.version = version;
+        this.brickCommunicator = brickCommunicator;
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response handle(JSONObject request) throws Exception {
-        int logLen = new ClientLogger().log(LOG, request);
+    public Response handle(@OraData HttpSessionState httpSessionState, JSONObject fullRequest) throws Exception {
+        int logLen = new ClientLogger().log(LOG, fullRequest);
         LOG.info("/ping");
         Date date = new Date();
-        JSONObject data = new JSONObject().put("version", this.version).put("date", date.getTime()).put("dateAsString", date.toString()).put("logged", logLen);
-        return Response.ok(data).build();
+        JSONObject response =
+            new JSONObject().put("version", this.version).put("date", date.getTime()).put("dateAsString", date.toString()).put("logged", logLen);
+        Util.addFrontendInfo(response, httpSessionState, this.brickCommunicator);
+        return Response.ok(response).build();
     }
 }
